58c9812f18df5e9d9535f7f13dcf5e6797f4779d
Aryan-bhatt25 <151805393+Aryan-bhatt25@users.noreply.github.com>
2024-02-11 01:54:47 +0000

    6:55 P.M.

---
+++ SingleNumber.java
@@ -0,0 +1,16 @@
+import java.util.*;
+
+class SingleNumber //class name 
+{
+    public static void main(String[] args) throws InterruptedException
+    {
+        Scanner sc = new Scanner(System.in); //calling the Scanner class
+
+    } //end of main
+
+    public int singleNumber(int[] nums) 
+    {
+        
+    } //end of singleNumber()
+
+} //end of class
