5ea3468d827f740844da3a4cc6f6628be65e20b1
Aryan-bhatt25 <151805393+Aryan-bhatt25@users.noreply.github.com>
2024-02-11 00:21:41 +0000

    5:21 P.M.

--- hello.java
+++ hello.java
@@ -1,9 +1,12 @@
 import java.util.*;
 
-class hello
+class hello //class name 
 {
     public static void main(String[] args)
     {
-       System.out.println("Hello"); 
-    }
-}
+
+       System.out.println("Hello"); //prints "hello"
+
+    } //end of main
+    
+} //end of class 
